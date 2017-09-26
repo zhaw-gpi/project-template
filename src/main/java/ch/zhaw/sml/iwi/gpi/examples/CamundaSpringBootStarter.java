5ff2c295cd4ac2224ffcbdf309fd80a9375c051e
scepbjoern <scep@zhaw.ch>
2017-09-26 18:06:59 +0200

    Von TwitterDemoProzess zu ExampleProzess (Hallo Welt) gewechselt
    - TwitterDemoProzess enthielt bereits zu viel Ballast, um als Projekt-
    template sinnvoll genutzt werden zu können
    - Zudem Code nochmals besser dokumentiert

--- src/main/java/ch/zhaw/sml/iwi/gpi/examples/CamundaSpringBootStarter.java
+++ src/main/java/ch/zhaw/sml/iwi/gpi/examples/CamundaSpringBootStarter.java
@@ -8,32 +8,39 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 /**
- * Ruft Camunda Spring Boot Starter auf, welches zusammengefasst folgende Schritte durchläuft:
- * 1. Tomcat initialisieren
- * 2. Camunda REST API aktivieren
- * 3. Camunda Job Executor initialisieren
- * 4. Camunda Process Engine inklusive Datenbank initialisieren gemäss application.properties und application.yaml
- * 5. Sofern noch nicht vorhanden, den Demo-Admin-User erstellen gemäss application.yaml
- * 6. Die Webapps (Tasklist, Admin, Cockpit) deployen
- * 7. Gefundene Prozesse (z.B. TwitterDemoProcess) deployen
- * 8. Alle Komponenten starten => unter localhost:8080 ist Tomcat erreichbar *
+ * Hauptklasse für die Prozessapplikation, welche diese mit allen Abhängigkeiten
+ * ausführt. Basis ist das SpringBoot-Framework, welches von Camunda erweitert
+ * wurde.
+ *
+ * Zusammengefasst werden dabei folgende Schritte durchlaufen: 0. Abhängigkeiten
+ * herunterladen 1. Tomcat initialisieren 2. Camunda REST API aktivieren 3.
+ * Camunda Job Executor initialisieren 4. Camunda Process Engine inklusive
+ * Datenbank initialisieren gemäss application.properties und application.yaml
+ * 5. Sofern noch nicht vorhanden, den Demo-Admin-User erstellen gemäss
+ * application.yaml 6. Die Webapps (Tasklist, Admin, Cockpit) deployen 7.
+ * Gefundene Prozesse (z.B. ExampleProcess) deployen 8. Alle Komponenten starten
+ * => unter localhost:8080 ist Tomcat erreichbar *
  */
 @SpringBootApplication
 @EnableProcessApplication
 public class CamundaSpringBootStarter {
 
     /**
-     * Main-Methode, welche die Spring-Application aufruft
-     * @param args 
+     * Haupt-Methode, welche beim Build/Run-Befehl eine
+     * Camunda-Prozessapplikation erstellt.
+     *
+     * @param args
      */
     public static void main(String[] args) {
         SpringApplication.run(CamundaSpringBootStarter.class, args);
     }
 
     /**
-     * Stellt sicher, das die H2-Konsole über http://localhost:8080/console aufrufbar ist
-     * 
-     * @return 
+     * Stellt sicher, das die H2-Konsole über http://localhost:8080/console
+     * aufrufbar ist Die H2-Konsole wird verwendet, um auf die Camunda-Datenbank
+     * per GUI zugreifen zu können.
+     *
+     * @return registration
      */
     @Bean
     public ServletRegistrationBean h2servletRegistration() {
---
+++ src/test/java/ch/zhaw/sml/iwi/gpi/examples/ExampleProcessApplicationTest.java
@@ -0,0 +1,26 @@
+package ch.zhaw.sml.iwi.gpi.examples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Klasse, um die Prozessapplikation automatisiert zu testen
+ *
+ * @author scep
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleProcessApplicationTest {
+
+    /**
+     * Beispieltest, der allerdings aktuell nichts testet
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void exampleTest() throws Exception {
+        // Hier würden Testanweisungen stehen
+    }
+}
